f1347601116155b25a1e448592804ab64ad097ce
nalcala <nalcala@stanford.edu>
2017-06-14 10:57:28 +0200

    added deployment script for circle ci

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$CIRCLE_BRANCH\"}" -X POST https://registry.hub.docker.com/u/iarcbioinfo/alignment-nf/trigger/5360932c-62ae-4969-9581-312f4089660e/ 
