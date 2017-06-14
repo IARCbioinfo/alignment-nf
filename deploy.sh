d4a6c0d5b9c68e68d6b4fa3ce5a4860083d56001
nalcala <nalcala@stanford.edu>
2017-06-14 15:48:44 +0200

    add dag automatic commit after circle ci

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+cd ~/alignment-nf/
+git add dag.png
+git add dag.html
+git commit -m "Generated DAG [skip ci]"
+git push origin $CIRCLE_BRANCH
+
 curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$CIRCLE_BRANCH\"}" -X POST https://registry.hub.docker.com/u/iarcbioinfo/alignment-nf/trigger/5360932c-62ae-4969-9581-312f4089660e/ 
+
+
