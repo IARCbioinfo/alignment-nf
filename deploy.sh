416331a6f660b39d46d682f5363b9bd92937fffb
nalcala <alcalan@fellows.iarc.fr>
2019-01-10 15:55:08 +0100

    update to circleCI 2.0

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-cd ~/alignment-nf/
+cd ~/project/
+commitID=`git log -n 1 --pretty="%h" -- environment.yml`
+sed -i '/^# environment.yml/d' Singularity && echo -e "\n# environment.yml commit ID: $commitID\n" >> Singularity
 git config --global user.email "alcalan@fellows.iarc.fr"
+git config --global user.name "Circle CI_$CIRCLE_PROJECT_REPONAME_$CIRCLE_BRANCH"
 git add dag.png
 git add dag.html
-git commit -m "Generated DAG [skip ci]"
+git status
+git commit -m "circle CI deployment [skip ci]"
 git push origin $CIRCLE_BRANCH
 
 curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$CIRCLE_BRANCH\"}" -X POST https://registry.hub.docker.com/u/iarcbioinfo/alignment-nf/trigger/5360932c-62ae-4969-9581-312f4089660e/ 
